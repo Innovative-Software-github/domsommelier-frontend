d7e9a71c4ec7ff60da80098c089b2ac12ce31d23
Владимир Крынин <krinin.vlad2804@icloud.com>
2025-03-13 22:59:09 +0300

    feature/add-customFetch: add customFetch beta

---
+++ src/services/config/customFetch.ts
@@ -0,0 +1,151 @@
+import { TAPIError } from '@/store/interfaces';
+import { getBackendHost } from '@/utils/getBackendHost';
+import { stringifySearchParams } from '@/utils/stringifySearchParams';
+
+export type THTTPRequestMethod =
+  | 'HEAD'
+  | 'GET'
+  | 'POST'
+  | 'PUT'
+  | 'PATCH'
+  | 'DELETE';
+export type TURIScheme = 'http' | 'https';
+
+export interface IHttpRequestConfig {
+  path: string;
+  method: THTTPRequestMethod;
+  headers?: [string, string][];
+  scheme?: TURIScheme;
+  port?: number;
+  contentType?: string;
+  withCredentials?: boolean;
+  withErrorHandling?: boolean;
+  signal?: AbortSignal;
+}
+
+export type RequestDataValue =
+  | string
+  | number
+  | null
+  | boolean
+  | void
+  | Array<RequestDataValue>;
+
+export interface IRequestData {
+  [key: string]: RequestDataValue | IRequestData;
+}
+
+export interface IResponse<T> {
+  data: T | null;
+  error: TAPIError;
+}
+
+/**
+ * Универсальная кастомная функция с irma-заголовками для выполнения HTTP-запросов на базе fetch
+ *
+ * @template TResponsePayload - Тип данных ответа
+ * @template TRequestPayload - Тип данных запроса
+ *
+ * @param {IHttpRequestConfig} settings - Настройки запроса
+ * @param {string} settings.path - Путь запроса
+ * @param {THTTPRequestMethod} settings.method - HTTP метод
+ * @param {[string, string][]} [settings.headers] - Дополнительные заголовки
+ * @param {TURIScheme} [settings.scheme] - Протокол (http/https)
+ * @param {number} [settings.port] - Порт
+ * @param {string} [settings.contentType] - Тип контента
+ * @param {boolean} [settings.withCredentials] - флаг отправлять ли куки
+ * @param {boolean} [settings.withErrorHandling] - флаг включить ли обработку ошибок
+ * @param {AbortSignal} [settings.signal] - Сигнал для отмены запроса
+ *
+ * @param {TRequestPayload} [data] - Данные для отправки
+ *
+ * @returns {Promise<IResponse<TResponsePayload> | TResponsePayload>} Результат запроса
+ * - Если withErrorHandling=true, возвращает объект {data, error}
+ * - Если withErrorHandling=false, возвращает данные напрямую
+ *
+ * @throws {Error} Выбрасывает ошибку, если не задан хост бэкенда
+ */
+export function customFetch<
+  TResponsePayload = unknown,
+  TRequestPayload = IRequestData,
+>(
+  settings: IHttpRequestConfig & { withErrorHandling: true },
+  data?: TRequestPayload,
+): Promise<IResponse<TResponsePayload>>;
+
+// eslint-disable-next-line no-redeclare
+export function customFetch<
+  TResponsePayload = unknown,
+  TRequestPayload = IRequestData,
+>(
+  settings: IHttpRequestConfig & { withErrorHandling?: false },
+  data?: TRequestPayload,
+): Promise<TResponsePayload>;
+
+// eslint-disable-next-line no-redeclare
+export async function customFetch<
+  TResponsePayload = unknown,
+  TRequestPayload = IRequestData,
+>(
+  {
+    path,
+    method,
+    headers = [],
+    scheme = 'https',
+    port,
+    contentType = 'application/json',
+    withCredentials = true,
+    withErrorHandling = false,
+    signal,
+  }: IHttpRequestConfig,
+  data?: TRequestPayload,
+): Promise<IResponse<TResponsePayload> | TResponsePayload> {
+  const makeRequest = async (): Promise<TResponsePayload> => {
+    const host = getBackendHost();
+
+    if (!host) {
+      throw new Error('Backend host is not defined');
+    }
+
+    const query =
+      data && method === 'GET' ? `?${stringifySearchParams(data)}` : '';
+    const body = data && method !== 'GET' ? JSON.stringify(data) : undefined;
+
+    const url = `${scheme}://${host}${port ? `:${port}` : ''}${path}${query}`;
+
+    const response = await fetch(url, {
+      method,
+      body,
+      headers: [...headers, ['Content-Type', contentType]],
+      credentials: withCredentials ? 'include' : 'omit',
+      cache: 'no-store',
+      signal,
+    });
+
+    if (!response.ok) {
+      const error = await response.json().catch(() => ({}));
+
+      throw error;
+    }
+
+    return response.json();
+  };
+
+  if (withErrorHandling) {
+    try {
+      const response = await makeRequest();
+
+      return {
+        data: response,
+        error: null,
+      };
+    } catch (e) {
+      return {
+        data: null,
+        error: e as TAPIError,
+      };
+    }
+  }
+
+  return makeRequest();
+}
---
+++ src/utils/stringifySearchParams.ts
@@ -0,0 +1,19 @@
+export function stringifySearchParams<T extends { [key: string]: any }>(
+  params: T
+): string {
+  return Object.keys(params)
+    .reduce((acc: string[], name) => {
+      if (!params[name]) {
+        return acc;
+      }
+
+      const arr = Array.isArray(params[name])
+        ? params[name].map(
+            (value: string) => `${name}=${encodeURIComponent(value)}`
+          )
+        : [`${name}=${encodeURIComponent(params[name])}`];
+
+      return [...acc, ...arr];
+    }, [])
+    .join("&");
+}
