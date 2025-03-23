24fa42f8f370e4d6fd12a2f029653ce60f5f7501
Владимир Крынин <krinin.vlad2804@icloud.com>
2025-03-24 00:21:34 +0300

    mainpage-news: create and connect component

---
+++ src/app/_components/NewsSwiper/NewsSwiper.tsx
@@ -0,0 +1,5 @@
+import cls from './NewsSwiper.module.scss';
+
+export const NewsSwiper: React.FC = () => {
+  return <section className={cls.container}>NewsSwiper</section>;
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Layout } from '@/ui/Layout/Layout';
 import { ContentContainer } from '../ui/ContentContainer/ContentContainer';
 import { CategoryLinks } from './_components/CategoryLinks/CategoryLinks';
+import { NewsSwiper } from './_components/NewsSwiper/NewsSwiper';
 
 export default function Home() {
   return (
     <Layout>
       <ContentContainer>
         <CategoryLinks />
+        <NewsSwiper />
       </ContentContainer>
     </Layout>
   );
