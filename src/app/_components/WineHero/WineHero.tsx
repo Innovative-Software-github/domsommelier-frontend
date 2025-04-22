d7c23c8504546b0937792f16aac135608f42d8c0
Крынин Владимир Юрьевич <vkrynin@116605-mba.local>
2025-04-22 22:47:00 +0300

    feat: add switch ui beta

--- src/app/_components/WineHero/WineHero.tsx
+++ src/app/_components/WineHero/WineHero.tsx
@@ -1,8 +1,12 @@
+'use client'
 import Image from 'next/image';
 import { ContentContainer } from '../../../ui/ContentContainer/ContentContainer';
+import { Switch } from '../../../ui/Switch/Switch';
 import cls from './WineHero.module.scss';
+import React from 'react';
 
 export const WineHero: React.FC = () => {
+  const [checked, setChecked] = React.useState(false);
   return (
     <section>
       <ContentContainer className={cls.container}>
@@ -19,6 +23,7 @@ export const WineHero: React.FC = () => {
         <span className={cls.letter}>N</span>
         <span className={cls.letter}>E</span>
       </ContentContainer>
+      <Switch className={cls.switch} size='medium' onChange={() => setChecked(prev => !prev)} checked={checked} variant="thin" />
     </section>
   );
 };
---
+++ src/ui/Switch/Switch.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import { clsx } from 'clsx';
+
+import cls from './Switch.module.scss';
+import { Icon } from '../Icon/Icon';
+import { IconType } from '../Icon/IconsMapping';
+
+type TLabelPosition = 'start' | 'end';
+type TSwitchSize = 'small' | 'medium';
+type TSwitchVariant = 'default' | 'thin' | 'square';
+
+type TSwitchProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'checked' | 'size' | 'id'> & {
+  checked: boolean;
+  size?: TSwitchSize;
+  variant?: TSwitchVariant;
+  label?: string;
+  labelClassName?: string;
+  labelPosition?: TLabelPosition;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const DISPLAY_NAME = 'Switch';
+
+export const Switch: React.FC<TSwitchProps> = (props) => {
+  const {
+    className,
+    checked,
+    disabled,
+    size = 'medium',
+    variant = 'default',
+    label,
+    labelClassName,
+    labelPosition = 'end',
+    onChange,
+    onMouseEnter,
+    onMouseLeave,
+  } = props;
+
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const uniqId = React.useId();
+
+  const renderLabel = () => {
+    if (!label) {
+      return null;
+    }
+
+    return (
+      <label htmlFor={uniqId} className={clsx(cls.label, labelClassName)}>
+        {label}
+      </label>
+    );
+  };
+
+  return (
+    <span
+      className={clsx(cls.wrapper, className, {
+        [cls.checked]: checked,
+        [cls.disabled]: disabled,
+      })}
+      data-size={size}
+      data-variant={variant}
+    >
+      {labelPosition === 'start' && (
+        renderLabel()
+      )}
+      <span className={cls.switchControlWrapper} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+        <input
+          ref={inputRef}
+          className={cls.switchControl}
+          id={uniqId}
+          type="checkbox"
+          checked={checked}
+          disabled={disabled}
+          onChange={onChange}
+        />
+        <span
+          className={cls.circle}
+          onClick={() => {
+            inputRef.current?.click();
+          }}
+        />
+      </span>
+      {labelPosition === 'end' && (
+        renderLabel()
+      )}
+    </span>
+  );
+};
+
+Switch.displayName = DISPLAY_NAME;
