c8a0f5347ffdaa6daa4c272e49f54553eb3720f6
Крынин Владимир Юрьевич <krinin.vlad2804@icloud.com>
2025-09-27 19:54:51 +0300

    (feat): update basket counter in header

--- src/components/Header/HeaderMainContent/IconLink/IconLink.tsx
+++ src/components/Header/HeaderMainContent/IconLink/IconLink.tsx
@@ -2,16 +2,31 @@ import { Icon } from '@/ui/Icon/Icon';
 import { IconType } from '@/ui/Icon/IconsMapping';
 import Link from 'next/link';
 import React from 'react';
+import cls from './IconLink.module.scss';
 
 export interface IIconLinkProps {
   href: string;
   iconType: IconType;
+  badgeCount?: number;
+  showBadge?: boolean;
 }
 
-export const IconLink: React.FC<IIconLinkProps> = ({ href, iconType }) => {
+export const IconLink: React.FC<IIconLinkProps> = ({ 
+  href, 
+  iconType, 
+  badgeCount = 0, 
+  showBadge = false 
+}) => {
   return (
-    <Link href={href}>
-      <Icon type={iconType} width={24} height={24} />
+    <Link href={href} className={cls.link}>
+      <div className={cls.iconContainer}>
+        <Icon type={iconType} width={24} height={24} />
+        {showBadge && badgeCount > 0 && (
+          <span className={cls.badge}>
+            {badgeCount > 99 ? '99+' : badgeCount}
+          </span>
+        )}
+      </div>
     </Link>
   );
 };
