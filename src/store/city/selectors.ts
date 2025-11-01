cb91a8fb60d73f858807a583dd5da5c4f82dcebc
Владимир Крынин <krinin.vlad2804@icloud.com>
2025-11-01 11:44:35 +0300

    (fix): fix build;

--- src/store/city/selectors.ts
+++ src/store/city/selectors.ts
@@ -1,5 +1,4 @@
 import { IStore } from '../interfaces';
 
-export const currentCitySelector = (state: IStore) => state.city.currentCity;
-export const citiesSelector = (state: IStore) => state.city.cities;
-
+export const currentCitySelector = (state: IStore) => state.city?.currentCity;
+export const citiesSelector = (state: IStore) => state.city?.cities || [];
