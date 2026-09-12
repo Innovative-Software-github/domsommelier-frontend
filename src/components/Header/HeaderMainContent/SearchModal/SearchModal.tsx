dc5160bb37795678ac519ecf689fdeffb3a65e79
Крынин Владимир Юрьевич <krinin.vlad2804@icloud.com>
2026-09-12 15:13:00 +0300

    fix: close search modal on backdrop click

--- src/components/Header/HeaderMainContent/SearchModal/SearchModal.tsx
+++ src/components/Header/HeaderMainContent/SearchModal/SearchModal.tsx
@@ -5,7 +5,6 @@ import { Backdrop } from '../../../../ui/Backdrop/Backdrop';
 import { ContentContainer } from '../../../../ui/ContentContainer/ContentContainer';
 import { SearchModalHeader } from './SearchModalHeader/SearchModalHeader';
 import { SearchModalBody } from './SearchModalBody/SearchModalBody';
-import { useBackdropClose } from '../../../../hooks/useBackdropClose';
 
 export interface ISearchModalProps {
   isOpen: boolean;
@@ -18,37 +17,15 @@ export const SearchModal: React.FC<ISearchModalProps> = ({
 }) => {
   const [query, setQuery] = React.useState('');
 
-  const backdropRef = useBackdropClose(onClose);
-
   React.useEffect(() => {
     if (!isOpen) {
       setQuery('');
     }
   }, [isOpen]);
 
-  const handleKeyDown = React.useCallback(
-    (e: KeyboardEvent) => {
-      if (e.key === 'Escape') {
-        onClose();
-      }
-    },
-    [onClose],
-  );
-
-  React.useEffect(() => {
-    if (isOpen) {
-      document.addEventListener('keydown', handleKeyDown);
-    } else {
-      document.removeEventListener('keydown', handleKeyDown);
-    }
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [isOpen, handleKeyDown]);
-
   return (
     <Backdrop
-      ref={backdropRef}
+      onClickCancelIcon={onClose}
       backdropClassName={cls.backdrop}
       contentClassName={cls.content}
       isOpen={isOpen}
